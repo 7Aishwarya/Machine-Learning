0d9d6ec168ca9a078a21d02c8eb82a15a9b034a5
Aishwarya <7aishwaryasharma@gmail.com>
2019-08-08 12:03:45 +0530

    eclat

---
+++ Part5 - Association Rule Learning/Apriori/apriori.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 00:16:19 2019
+
+@author: Aishwarya
+"""
+
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training Apriori on the dataset
+from Apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules)
